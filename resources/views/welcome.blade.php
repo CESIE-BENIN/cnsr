08c4d777b91ce4b85dd06b7bd92351f5e2d54104
EribertDev <eribertganhoun7@gmail.com>
2025-09-23 09:54:22 +0100

    Cnsr_Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
       
-        <title>TEST</title>
+        <title>@yield('title') </title>
         <!-- Latest Bootstrap min CSS -->
        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
+        <link rel="shortcut icon" href="{{ asset('image\cnsr.jpg') }}" type="image/x-icon">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @yield('extra-style')
     </head>
